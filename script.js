233491555562222ce3b75b90e073d7284b4b39cf
Jack <jackpeterson99528@gmail.com>
2023-05-18 14:35:47 -0400

    Fixed up api fetch

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
+
+
 function weatherAPI() {
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=77.0369&lon=38.9072&appid=09e914382d323bf6c810a1d44a6f5e0c"
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=${&lon=38.9072&appid=09e914382d323bf6c810a1d44a6f5e0c"
     fetch(queryURL)
     .then(function(response) {
         return response.json();
